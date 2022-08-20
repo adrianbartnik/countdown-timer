fcf2495e54cd995b94ec0233c4a1b839974a1b22
Adrian Bartnik <adrian.bartnik@netlight.com>
2022-08-20 17:52:22 +0200

    Deploy using yarn

--- next.config.js
+++ next.config.js
@@ -5,7 +5,12 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? '/countdown-timer/' : ''
+  assetPrefix: isProd ? '/countdown-timer/' : '',
+  experimental: {
+    images: {
+      unoptimized: true,
+    }
+  }
 }
 
 module.exports = nextConfig
