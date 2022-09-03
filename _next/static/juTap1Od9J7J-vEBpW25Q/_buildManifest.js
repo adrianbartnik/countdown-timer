21420dfc00be3f1fa1bf1d52e118d92a56495851
adrianbartnik <adrianbartnik@users.noreply.github.com>
2022-09-03 14:03:05 +0000

    deploy: 5b18735c28e0160862bd1c07d63a02c79cc44396

--- _next/static/juTap1Od9J7J-vEBpW25Q/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/176-9d6e5262ec11d6d6.js","static/css/b7e966551114ae11.css","static/chunks/pages/index-2471af3bfb1a166c.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
